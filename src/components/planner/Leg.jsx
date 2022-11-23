2fed50123e27a49bceaa113fd4d6e7b974bf446b
Esther2504 <esther252000@gmail.com>
2022-11-23 20:15:19 +0100

    grid

--- src/components/planner/Leg.jsx
+++ src/components/planner/Leg.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import personfill from "./../../personfill.svg";
+import personnofill from "./../../personnofill.svg";
+import person3 from "./../../person3.svg";
 
 export default function Leg({ leg }) {
   const today = new Date(leg.origin.plannedDateTime);
@@ -9,88 +12,128 @@ export default function Leg({ leg }) {
 
   return (
     <Wrapper>
-        
-              <div class="vl"></div>
-    <Trip>
-      
-
+      {/* <div class="vl"></div> */}
+      <Trip>
         <StartStation>
-        <p>{time}</p>
-    
-          <p className="stationname">{leg.origin.name}</p>
-          <p>Spoor {leg.origin.plannedTrack}</p>
+          <Time>{time}</Time>
+
+          <StationName>{leg.origin.name}</StationName>
+          <Track>Spoor {leg.origin.plannedTrack}</Track>
         </StartStation>
         <TripInfo>
-       <p>hi</p>
-            <Train>   <p>{leg.product.displayName}</p>
-          <p>Richting {leg.direction}</p></Train>
-       
+          <p></p>
+          <Train>
+            <p>{leg.product.displayName}</p>
+            <p>Richting {leg.direction}</p>
+          </Train>
 
           {/* {leg.stops.map((stop) => {
             return <span>{stop.name}</span>
           })} */}
-          <p>{leg.crowdForecast}</p>
+          {/* <p>{leg.crowdForecast}</p> */}
+          <div>
+            {leg.crowdForecast === "LOW" ? (
+              <>
+                <img src={personfill} />
+                <img src={personnofill} />
+                <img src={personnofill} />
+              </>
+            ) : leg.crowdForecast === "MEDIUM" ? (
+              <>
+                <img src={personfill} />
+                <img src={personfill} />
+                <img src={personnofill} />
+              </>
+            ) : leg.crowdForecast === "HIGH" ? (
+              <>
+                <img src={personfill} />
+                <img src={personfill} />
+                <img src={personfill} />
+              </>
+            ) : leg.crowdForecast === "UNKNOWN" ? (
+              <>
+                <img src={personfill} />?
+              </>
+            ) : null}
+          </div>
         </TripInfo>
-   
 
-      
-      <EndStation>
-      <p>{time2}</p>
-      <p className="stationname">{leg.destination.name}</p>
-      <p>Spoor {leg.destination.plannedTrack}</p>
-      {/* <p>Zijde {leg.destination.exitSide}</p> */}
-      </EndStation>
+        <EndStation>
+          <Time>{time2}</Time>
+
+          <StationName>{leg.destination.name}</StationName>
+          <Track>Spoor {leg.destination.plannedTrack}</Track>
+          {/* <p>Zijde {leg.destination.exitSide}</p> */}
+        </EndStation>
       </Trip>
       {/* <p>{leg.cancelled}</p> */}
       {/* <p>{leg.plannedDurationInMinutes} minutes</p> */}
-      ---
+      <br></br>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-display: flex;
-flex-direction: column;
-width: 600px;
+  display: grid;
+  flex-direction: column;
+  width: 600px;
 
-.vl {
+  .vl {
     border-left: 2px solid purple;
     height: 180px;
     position: absolute;
   }
-`
+`;
+
+const Time = styled.p`
+  line-height: 45px;
+  border-right: 1px solid white;
+`;
+const StationName = styled.p`
+  font-size: 3rem;
+  margin-left: 10px;
+`;
+const Track = styled.p`
+  line-height: 45px;
+`;
 
 const Trip = styled.div`
-
-`
+  border-bottom: 1px solid white;
+`;
 
 const StartStation = styled.div`
-  display: flex;
+  display: grid;
   width: 100%;
   justify-content: space-between;
+  border-bottom: 1px solid white;
+  grid-template-columns: 50px 490px 200px;
 
   .stationname {
     font-size: 3rem;
+    margin-left: 10px;
   }
 `;
 
-const Train = styled.div`
-
-`
+const Train = styled.div``;
 
 const EndStation = styled.div`
-  display: flex;
+  display: grid;
   width: 100%;
-  justify-content: space-between;
+  justify-content: space-evenly;
+  // border-bottom: 1px solid white;
+  grid-template-columns: 50px 490px 200px;
 
   .stationname {
     font-size: 3rem;
+    margin-left: 10px;
   }
 `;
 const TripInfo = styled.div`
-  display: flex;
-  width: 60%;
-  justify-content: space-between;
+  display: grid;
+  width: 100%;
+  justify-content: space-evenly;
+  // border-bottom: 1px solid white;
+  grid-template-columns: 50px 490px 200px;
 
   .stationname {
     font-size: 3rem;
